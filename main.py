d660d8943414b7253b92c26df6b102a637cc3569
Aleksis Boters 211RDB214 <125483680+aflexxxx@users.noreply.github.com>
2023-02-19 16:54:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,11 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
     if text[0]=="I":
-      print(mismatch)
+        text = input()
+        mismatch = find_mismatch(text)
+    # Printing answer, write your code here
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
